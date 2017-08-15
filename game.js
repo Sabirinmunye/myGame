dcb37ad78ea2f4ad4444960dfa64a02d8e190e35
Sabirin Munye <sabirinmunye@gmail.com>
2017-08-15 15:26:59 +0100

    5x5 game update

--- game.js
+++ game.js
@@ -17,12 +17,10 @@ var counter = 0;
 var winCounter = 0;
 
 //winning combinations array 
-var winningCombinations = [[0,1,2],[3,4,5],[6,7,8], [0,3,6], [1,4,7], [2,5,8], [0,4,8],[2,4,6]]
-
+var winningCombinations = [[0,1,2,3],[1,2,3,4],[5,6,7,8],[6,7,8,9],[10,11,12,13],[11,12,13,14],[15,16,17,18],[16,17,18,19],[20,21,22,23],[21,22,23,24],[0,5,10,15],[5,10,15,20],[1,6,11,16],[6,11,16,21],[2,7,12,17],[7,12,17,22],[3,8,13,18],[8,13,18,23],[4,9,14,19],[9,14,19,24],[5,11,17,23],[0,6,12,18],[6,12,18,24],[1,7,13,19],[15,11,7,3],[21,17,13,9],[20,16,12,8],[16,12,8,4]]
 //arrays to track the movesof players
-var redMoves = [''];
-var yellowMoves = [''];
-
+var redMoves = [];
+var yellowMoves = [];
 
 // function to run  when page loads
 function start() {
@@ -30,6 +28,13 @@ function start() {
 		addResetListener()
 }
 	
+//FUNCTION TO SET UP DROP BOX LISTENERS
+function addDropListeners() {
+		for (var i=boxes.length-1; i>=0;i--) {
+			var $box = $($boxes[i]);
+			$box.on('click', addRedorYellow)
+		}
+	} 
 
 //the function to set up red and yellow Listeners
 function addRedandYellowListeners(){
@@ -40,7 +45,7 @@ function addRedandYellowListeners(){
 }
 
 
-// function to remove the all event listeners 
+// function to remove all event listeners 
 
 function removeRedandYellowListeners(){
 	for (var i = $boxes.length -1; i>=0;i--){
@@ -54,38 +59,50 @@ function addResetListener() {
 	$('#reset').on('click', resetBoard)
 }
 
+function setBoxRedOrYellow(box) {
+	if(counter % 2 === 0){
+		redMoves.push(parseInt(box.attr('box-coord')));
+		box.attr('class', 'red');
+		$turnText.html("Its Yellows's Turn");
+		checkWin(redMoves, 'red')
+		counter++;
+	} else {
+		yellowMoves.push(parseInt(box.attr('box-coord')));
+		box.attr('class', 'yellow');
+		$turnText.html("Its Red's Turn");
+		checkWin(yellowMoves, 'yellow')
+		counter++;
+	} if (counter >= 25) {
+			$turnText.html('game over, Its a draw')
+	}
+}
+
 
 // function to add red or yellow for choices
 function addRedorYellow() {
-	var $box = $(this);
-	console.log($box);
+	console.log('running')
+	var $dropCell = $(this);
+	var $boxes = $('td');
+	var $colHead = $('th');
+	var index = $dropCell[0].cellIndex;
+	var $box1 = $($boxes[index+20]);
+	var $box2 = $($boxes[index+15]);
+	var $box3 = $($boxes[index+10]);
+	var $box4 = $($boxes[index+5]);
+	var $box5 = $($boxes[index+0]);
 
-		if (counter % 2 ===0) {
-			
-			redMoves.push(parseInt($box.attr('box-coord')));
-			$box.attr('class', 'red')
-			console.log(redMoves);
-			$turnText.html("its Yellows's Turn");
-			counter++;
-			checkWin(redMoves, 'red')
-
-		}else {
-			yellowMoves.push(parseInt($box.attr('box-coord')));
-			console.log(yellowMoves);
-			$box.attr('class', 'yellow')
-			$turnText.html("its red's Turn");
-			counter++;
-			checkWin(yellowMoves, 'yellow')
-
-
-		}
-		//cater for draw if counter >=9 it is a drar
-		if (counter >= 9) {
-			$turnText.html('game over, Its a draw')
-		}
-
-
+		if ($box1.hasClass("")) {
+			setBoxRedOrYellow($box1)
+		} else if ($box2.hasClass("")) {
+			setBoxRedOrYellow($box2)
+	}	else if ($box3.hasClass("")) {
+			setBoxRedOrYellow($box3)
+	}	else if ($box4.hasClass("")) {
+			setBoxRedOrYellow($box4)
+	}	else if ($box5.hasClass("")) {
+			setBoxRedOrYellow($box5)
 	}
+}
 
 // function to check the player has won
 
@@ -97,7 +114,7 @@ function addRedorYellow() {
         if(movesArray.indexOf(winningCombinations[i][j]) !== -1){
           winCounter++
         }
-        if(winCounter === 3) {
+        if(winCounter === 4) {
           $turnText.html('Game Over,' + name + ' wins!');
           removeRedandYellowListeners();
         }
@@ -121,20 +138,7 @@ function resetBoard() {
 	addRedandYellowListeners()
 	$turnText.html("it is red's turn")
 }
-function sound(src) {
-    this.sound = document.createElement("audio");
-    this.sound.src = src;
-    this.sound.setAttribute("preload", "auto");
-    this.sound.setAttribute("controls", "none");
-    this.sound.style.display = "none";
-    document.body.appendChild(this.sound);
-    this.play = function(){
-        this.sound.play();
-    }
-    this.stop = function(){
-        this.sound.pause();
-    }
-}''
+
 
 
 start();
